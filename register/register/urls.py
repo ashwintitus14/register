f3194853e847afe9af7b2af8f4f67136720dcc32
Ashwin Titus <ashwintitus14@gmail.com>
2019-07-03 18:34:19 +0530

    fixed urls.py

--- register/register/urls.py
+++ register/register/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.views.generic import RedirectView
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studreg/', include('studereg.urls'),)
+    path('studreg/', include('studereg.urls'),),
     path('', RedirectView.as_view(url='/studreg/', permanent=True)), # Redirects root URL to /studreg/
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT), #Enabled serving of static files
 ]
