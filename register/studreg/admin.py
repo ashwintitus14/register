f92ddf942332441244f7bb257321fe89d992eb4b
Ashwin Titus <ashwintitus14@gmail.com>
2019-07-03 23:39:22 +0530

    changes

--- register/studreg/admin.py
+++ register/studreg/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from studreg.models import Student
+
 # Register your models here.
+admin.site.register(Student)  # Imports a data model and registers it
--- register/studreg/models.py
+++ register/studreg/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 
@@ -48,9 +50,9 @@ class Student(models.Model):
     g_address = models.TextField("Guardian's Address")
     g_phone = models.CharField("Guardian's Phone Number", max_length=20, help_text="Don't use spaces in the phone number.")
     g_email = models.EmailField("Guardian's Email ID")
-    g_relationship = models.CharField("Guardian's relationship with the student. (If 'Other' selected)", blank=True, null=True)
+    g_relationship = models.CharField("Guardian's relationship with the student. (If 'Other' selected)", max_length=20, blank=True, null=True)
 
-    local_guardian_name = models.CharField('Name of Local Guardian (essential in case of students outside Thiruvananthapuram', max_length=100)
+    local_guardian_name = models.CharField('Name of Local Guardian (essential in case of students outside Thiruvananthapuram)', max_length=100)
     local_guardian_address = models.TextField('Address of Local Guardian')
 
     tc_number = models.CharField('Number of TC produced', max_length=50)
@@ -59,12 +61,12 @@ class Student(models.Model):
 
     exam_roll_no = models.CharField('Register number of qualifying exam', max_length=50)
 
-    admission_no = models.CharField('Admission Number', max_length=10)
+    admission_no = models.CharField('Admission Number', max_length=10, unique=True)
     tc_taken = models.BooleanField('Whether TC taken from GECBH', default=False)
 
     date_of_admission = models.DateField('Date of admission', auto_now_add=True)
 
-    entrance_roll_no = models.CharField('KEAM Entrance Roll number', max_length=7, primary_key=True)
+    entrance_roll_no = models.CharField('KEAM Entrance Roll number', max_length=7, primary_key=True, unique=True)
     entrance_rank = models.CharField('KEAM State Merit Rank', max_length=10)
     reservation_rank = models.CharField('Special category/Reservation Rank', max_length=10, blank=True, null=True)
 
@@ -74,4 +76,12 @@ class Student(models.Model):
     alloted_course_code = models.CharField('Alloted Course Code', max_length=5)
     alloted_branch_code = models.CharField('Alloted Branch Code', max_length=5)
     reservation_code = models.CharField('Reservation/Special Category Code', max_length=10)
-    
+
+    def __str__(self):
+        """String represeting the Student object."""
+        return self.entrance_roll_no
+
+    def get_absolute_url(self):
+        """Returns the url to access a detailed record for this student."""
+        return reverse('student-detail', args=[str(self.entrance_roll_no)]
+        )
