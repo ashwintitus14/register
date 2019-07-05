03d1a78d6d88f47ca7e867a9799dbe00810fbea7
Ashwin Titus <ashwintitus14@gmail.com>
2019-07-05 11:06:46 +0530

    Update settings.py

--- register/register/settings.py
+++ register/register/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'studreg', # Registered the studreg application
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
